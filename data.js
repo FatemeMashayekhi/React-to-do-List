c37e71ceb02e7061974b6e206991b7ad28bad961
Maral Mashayekhi <mashayekhimaral@gmail.com>
2024-07-18 14:53:54 +0330

    data added

---
+++ data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    name: "finish project",
+    is_Done: true,
+    id: 1,
+  },
+  {
+    name: "take out the trash",
+    is_Done: false,
+    id: 2,
+  },
+  {
+    name: "go to the doctor",
+    is_Done: true,
+    id: 3,
+  },
+  {
+    name: "wash dishes",
+    is_Done: false,
+    id: 4,
+  },
+];
