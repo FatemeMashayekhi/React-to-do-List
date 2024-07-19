3a89af54e3a000669f5175603f873fb744082d96
Maral Mashayekhi <mashayekhimaral@gmail.com>
2024-07-19 17:20:23 +0330

    fix : selectBox

--- src/components/MainTasks.jsx
+++ src/components/MainTasks.jsx
@@ -1,20 +1,24 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+
 import Header from "./Header/Header";
 import TaskContent from "./TaskContent";
 import { TaskManeger } from "../taskContext";
 
 export default function MainTasks() {
-  const {tasks, filterValue} =TaskManeger()
+  const { tasks, filterValue } = TaskManeger();
   return (
     <div className="bg-[#1b99e5] flex flex-col justify-center items-center gap-y-4 p-3">
       <p className="font-semibold text-2xl text-white">Tasks</p>
       <Header />
-      <TaskContent tasks={filterValue === "done"
-  ? tasks.filter((task) => task.is_Done)
-  : filterValue === "undone"
-  ? tasks.filter((task) => !task.is_Done)
-  : tasks} />
-      </div>
+      <TaskContent
+        tasks={
+          filterValue === "done"
+            ? tasks.filter((task) => task.is_Done)
+            : filterValue === "undone"
+            ? tasks.filter((task) => !task.is_Done)
+            : tasks
+        }
+      />
+    </div>
   );
 }
