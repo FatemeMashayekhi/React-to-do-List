68ec84633c903195827c0804b22bb194861eb170
Maral Mashayekhi <mashayekhimaral@gmail.com>
2024-07-19 14:48:00 +0330

    style : icons

--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -2,22 +2,21 @@
 import { Icon } from "@iconify/react";
 export default function TaskRow({ task }) {
   const { name } = task;
+  // const deleteTask = (id) => {
+  //   setProducts(products.filter((p) => p.id !== id));
+  // };
   return (
     <div>
       <div className="flex gap-x-2 items-center">
         <p className="bg-white p-1 text-left w-40">{name}</p>
-        <div className="bg-[#07bea4] p-1">
-          <Icon
-            icon="mdi:tick"
-            className="text-white text-2xl cursor-pointer"
-          />
-        </div>
-        <div className="bg-[#fa7a46] p-1">
-          <Icon
-            icon="ion:trash"
-            className="text-white text-2xl cursor-pointer"
-          />
-        </div>
+
+        <button className="bg-[#07bea4] p-1">
+          <Icon icon="mdi:tick" className="text-white text-2xl" />
+        </button>
+
+        <button className="bg-[#fa7a46] p-1">
+          <Icon icon="ion:trash" className="text-white text-2xl" />
+        </button>
       </div>
     </div>
   );
